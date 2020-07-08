ef214432dff2861734ef9cbac6cfa4d82250d4de
Yanyi Chen X <yanyi.x.chen@ericsson.com>
2020-07-08 12:35:28 +0800

    first commit

---
+++ src/test/java/info/galudisu/pg/LearningPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package info.galudisu.pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPostgresqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
